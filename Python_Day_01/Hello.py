da006a92a5ab932c29a80b2767c5aed429b843f8
AKD <contact.mousom@gmail.com>
2023-12-18 00:49:52 +0600

    My First Hello World Program

---
+++ Python_Day_01/Hello.py
@@ -0,0 +1,8 @@
+'''
+What is Python?
+Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+'''
+
+# How can i write a hello world program in one line.
+
+print("Hello world")
